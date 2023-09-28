71336266404b79b192161c8aa155f3846eddeaa2
David Litwin <davidglitwin@posteo.net>
2023-09-28 16:49:41 +0200

    fix find_contact, add plots

--- virtual_karst_lithology.py
+++ virtual_karst_lithology.py
@@ -7,11 +7,14 @@ and use the lithologic contact to determine where there will be springs.
 """
 
 #%%
+import copy
 import os
 import numpy as np
 
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from matplotlib.colors import LightSource
+
 from landlab import RasterModelGrid
 from landlab.components import (
     FastscapeEroder, 
@@ -84,13 +87,13 @@ def get_mask_and_large_channels(mg, bnds_lower, bnds_upper, area_thresh):
     return lower_mask, lower_channels, upper_channels
 
 
-def find_contact(mg, rock_id=1):
+def find_contact(mg, lith, rock_id=1):
     """
     Find the nodes at the contact between lithologies. Select the nodes on the 
     rock_id side of the contact. 
     """
     # calculate gradient of rock type
-    grad = mg.calc_grad_at_link("rock_type__id")
+    grad = mg.calc_grad_at_link((lith.dz[rock_id,:] > 1e-2)*1)
 
     # set links adjacent to boundaries to zero
     stat_nal = mg.status_at_node[mg.nodes_at_link]
@@ -107,8 +110,7 @@ def find_contact(mg, rock_id=1):
     return grad_intersect
     
 
-
-save_directory = '/Users/dlitwin/Documents/Research/karst_lem/landlab_virtual_karst/figures'
+save_directory = '/Users/dlitwin/Documents/Research/Karst landscape evolution/landlab_virtual_karst/figures'
 
 #%% parameters
 
@@ -130,24 +132,15 @@ z = mg.add_zeros("node", "topographic__elevation")
 np.random.seed(10010)
 z += 0.1*np.random.rand(len(z))
 
-
 # two layers, both with bottoms below ground. Top layer is limestone, bottom is basement.
 layer_elevations = [100,1000]
 layer_ids = [0,1]
-attrs = {"K_loss": {0: 0.2, 1: 0.0}}
-
+attrs = {"K_loss": {0: 0.5, 1: 0.0}}
 
 lith = LithoLayers(
     mg, layer_elevations, layer_ids, function=lambda x, y: 0.002 * y, attrs=attrs
 )
-
 mg.imshow("rock_type__id", cmap="viridis")
-
-
-#%% set up flow accumulator and loss
-
-
-q = mg.add_ones("node", "local_runoff")
 
 #%%
 # define the loss based on the lithology dependent K_loss field
@@ -222,6 +215,8 @@ def area_weighted_springs(mg, contact, Q_total, boundary_row=1, rock_id=1):
 
 #%% instantiate components 
 
+
+q = mg.add_ones("node", "local_runoff")
 fd2 = FlowDirectorD8(mg)
 fa2 = LossyFlowAccumulator(
     mg,
@@ -251,7 +246,7 @@ Q = mg.at_node['surface_water__discharge']
 
 #%% run forward
 
-N = 500
+N = 5000
 R = np.zeros(N)
 dt = 500
 
@@ -262,6 +257,11 @@ lower_bndry = np.arange(0, mg.number_of_node_columns)
 
 dz_ad = np.zeros(mg.size("node"))
 dz_ad[mg.core_nodes] = U * dt
+
+save_freq = 50
+Ns = N//save_freq
+z_all = np.zeros((len(z),Ns))
+rock_id_all = np.zeros((len(z),Ns))
 
 for i in range(N):
 
@@ -281,7 +281,7 @@ for i in range(N):
 
     # calculate spring discharge that will be used next time
     total_loss = np.sum(q_loss)
-    contact = find_contact(mg)
+    contact = find_contact(mg, lith)
     lower_drainage = get_divide_mask(mg, lower_bndry)
     lower_contact = np.logical_and(contact, lower_drainage)
     Q_spring = area_weighted_springs(mg, lower_contact, total_loss)
@@ -290,29 +290,124 @@ for i in range(N):
     # metrics of change
     R[i] = np.mean(z[mg.core_nodes])
 
-    if i%100==0:
+    if i%save_freq==0:
         print(f"Finished iteration {i}")
+
+        z_all[:,i//save_freq] = z
+        rock_id_all[:,i//save_freq] = mg.at_node['rock_type__id']
 
 # %% plot topogrpahic change
 
 # topography
 plt.figure()
 mg.imshow("topographic__elevation", colorbar_label='Elevation [m]')
-plt.savefig(os.path.join(save_directory,"litholayers_topog_springs.png"))
+# plt.savefig(os.path.join(save_directory,"litholayers_topog_springs_0.5.png"))
 
 plt.figure()
 mg.imshow("rock_type__id", cmap="viridis", colorbar_label='Rock ID')
-plt.savefig(os.path.join(save_directory,"litholayers_rocks_springs.png"))
+# plt.savefig(os.path.join(save_directory,"litholayers_rocks_springs_0.5.png"))
 
 plt.figure()
 mg.imshow('surface_water__discharge', cmap="plasma", colorbar_label='Discharge')
-plt.savefig(os.path.join(save_directory,"litholayers_discharge_springs.png"))
+# plt.savefig(os.path.join(save_directory,"litholayers_discharge_springs_0.5.png"))
+
+
+#%%
+
+Q_an = mg.at_node['surface_water__discharge']/mg.at_node['drainage_area']
+plt.figure()
+mg.imshow(contact, cmap="plasma", colorbar_label='Discharge')
+# plt.savefig(os.path.join(save_directory,"litholayers_discharge_springs_0.5.png"))
+
+#%%
+plt.figure()
+mg.imshow('local_runoff', cmap="plasma", colorbar_label='Discharge')
+#%%
+# hillshade
+
+elev = mg.at_node['topographic__elevation'].copy()
+elev[mg.boundary_nodes] = np.nan
+y = np.arange(mg.shape[0] + 1) * mg.dx - mg.dx * 0.5
+x = np.arange(mg.shape[1] + 1) * mg.dy - mg.dy * 0.5
+
+elev_plot = elev.reshape(mg.shape)
+elev_profile = np.nanmean(elev_plot, axis=1)
+
+f, (ax0, ax1) = plt.subplots(1, 2, width_ratios=[4, 1], figsize=(10,5))
+ls = LightSource(azdeg=135, altdeg=45)
+ax0.imshow(
+        ls.hillshade(elev_plot, 
+            vert_exag=1, 
+            dx=mg.dx, 
+            dy=mg.dy), 
+        origin="lower", 
+        extent=(x[0], x[-1], y[0], y[-1]), 
+        cmap='gray',
+        )
+for i in range(mg.shape[1]):
+    ax1.plot(elev_plot[:,i], y[0:-1], alpha=0.1, color='k')
+ax1.plot(elev_profile, y[0:-1], linewidth=2, color='r')
+
+ax0.set_xlabel('X [m]')
+ax0.set_ylabel('Y [m]')
+ax1.set_xlabel('Z [m]')
+f.tight_layout()
+# plt.savefig(os.path.join(save_directory,"litholayers_hillshade_springs_0.5.png"))
+
+#%%
+
+plt.figure()
+zplot = z_all[1]
+zplot[np.isnan(zplot)] = 0
+mg.imshow(zplot)
+
+#%%
+plt.figure()
+ridplot = rock_id_all[1]
+ridplot[np.isnan(ridplot)] = 0
+mg.imshow(ridplot)
+
+
+#%% save all
+
+for i in range(Ns):
+    elev = z_all[:,i]
+
+    elev[mg.boundary_nodes] = np.nan
+    y = np.arange(mg.shape[0] + 1) * mg.dx - mg.dx * 0.5
+    x = np.arange(mg.shape[1] + 1) * mg.dy - mg.dy * 0.5
+
+    elev_plot = elev.reshape(mg.shape)
+    elev_profile = np.nanmean(elev_plot, axis=1)
+
+    f, (ax0, ax1) = plt.subplots(1, 2, width_ratios=[4, 1], figsize=(10,5))
+    ls = LightSource(azdeg=135, altdeg=45)
+    ax0.imshow(
+            ls.hillshade(elev_plot, 
+                vert_exag=1, 
+                dx=mg.dx, 
+                dy=mg.dy), 
+            origin="lower", 
+            extent=(x[0], x[-1], y[0], y[-1]), 
+            cmap='gray',
+            )
+    for j in range(mg.shape[1]):
+        ax1.plot(elev_plot[:,j], y[0:-1], alpha=0.1, color='k')
+    ax1.plot(elev_profile, y[0:-1], linewidth=2, color='r')
+
+    ax0.set_xlabel('X [m]')
+    ax0.set_ylabel('Y [m]')
+    ax1.set_xlabel('Z [m]')
+    f.tight_layout()
+
+    plt.savefig(os.path.join(save_directory,f"litholayers_hillshade_springs_0.5_%d.png"%i))
+    plt.close()
 
 # %% find the lithologic contact
 
 
 plt.figure()
-contact = find_contact(mg)
+contact = find_contact(mg, lith)
 mg.imshow(contact, cmap="plasma", colorbar_label='Rock contact')
 
 
@@ -326,4 +421,32 @@ plt.figure()
 mg.imshow(lower_contact, cmap="plasma", colorbar_label='Rock contact')
 
 
+# %%
+
+mg1 = copy.copy(mg)
+
+N = 4000
+
+elev_fun = lambda y, N, U, dt: 0.002 * y + N * U * dt - 100
+
+surface = elev_fun(mg1.y_of_node, N, U, dt)
+
+plt.figure()
+mg1.imshow("rock_type__id")
+
+
+elev_plot = z.copy()
+elev_plot[np.isnan(elev_plot)] = 0.0
+surface_plot = elev_plot > surface
+plt.figure()
+mg1.imshow(surface_plot)
+
+
+# %%
+plt.figure()
+mg.imshow(lith.dz[1,:]>0)
+
+# %%
+plt.figure()
+mg.imshow("rock_type__id")
 # %%
